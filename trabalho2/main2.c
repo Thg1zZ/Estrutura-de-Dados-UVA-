4d170f46bd6d97781fe048ece025b4b80b51314c
Thiago Gomes <165279597+Thg1zZ@users.noreply.github.com>
2025-03-19 10:53:02 -0300

    Add files via upload

---
+++ trabalho2/main2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	
+	int a,b;
+	
+	printf("Digite o primeiro valor 1:");
+	scanf("%d", &a);
+	printf("Digite o valor 2:");
+	scanf("%d", &b);
+	
+	printf("O valor 1: %d  e valor 2:%d",a, b);
+	
+	
+	
+	
+	return 0;
+}
